bb87f51398889c8fda9cb61f052a954e6c2ff1f0
TiareHo <tiarehoegerman@gmail.com>
2022-08-10 13:30:54 -0700

    Bootstrap installed, superfluous files deleted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+      <div className="container">
+      <h1>Weather Report
+      </h1>
+      <p>This page is open-sourced via <a class="btn btn-primary" href="https://getbootstrap.com/docs/4.0/components/buttons/">BootstrapTEST</a> and hosted on {" "}         
+         <a href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          rel="noopener noreferrer">
+          Netlify
+         </a>
+        </p>
+        </div>
     </div>
   );
 }
