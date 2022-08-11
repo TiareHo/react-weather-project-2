539668424ab0f551b44296f6ac72c4a9bd2b54fd
TiareHo <tiarehoegerman@gmail.com>
2022-08-11 09:39:04 -0700

    Added search engine state management

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Search.css';
 
-function Search() {
+export default function Search() {
+  
+  const [city, setCity]=useState("Santa Barbara");
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log({city});
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
     return( 
         <div className="container">
-            <form>
-              <input type="search" placeholder="Type a city..." id="userSearchedCity"/>
+            <form onSubmit={handleSubmit}>
+              <input type="search" placeholder="Type a city..." id="userSearchedCity" onChange={updateCity}/>
               <button type="submit"> > </button> 
                 </form>
-        </div>);
-    
-}
-
-export default Search;
+        </div>);}
